c15de13d046d1a47ca2e0c5fb4d92c6d3cc98b1f
Jose Padilla <jp06171@gmail.com>
2023-01-30 21:17:40 -0600

    initial commit

---
+++ client/src/assets/index.js
@@ -0,0 +1,6 @@
+import download from './download.png';
+import logo from './logo.svg';
+import preview from './preview.png';
+import logo_final from './logo_final.svg';
+
+export { download, logo, preview, logo_final };
